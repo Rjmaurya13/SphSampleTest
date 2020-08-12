a71ffa6088e4b2aa992218884080025ea56f5a7f
Rajesh <rajeshmaurya1307@gmail.com>
2020-08-12 13:55:41 +0800

    Pod file installed.

---
+++ SPHSampleTest/Pods/Target Support Files/Pods-SPHSampleTest-SPHSampleTestUITests/Pods-SPHSampleTest-SPHSampleTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPHSampleTest_SPHSampleTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SPHSampleTest_SPHSampleTestUITests
+@end
