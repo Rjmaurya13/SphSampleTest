8a0f7b5a392b816d1508dc0cd2a0b534938f942a
Rajesh <rajeshmaurya1307@gmail.com>
2020-08-12 13:51:24 +0800

    Initial commit.

---
+++ SPHSampleTest/SPHSampleTest/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SPHSampleTest
+//
+//  Created by Rajeshkumar on 12/8/20.
+//  Copyright © 2020 rajesh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SPHSampleTest/SPHSampleTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SPHSampleTest
+//
+//  Created by Rajeshkumar on 12/8/20.
+//  Copyright © 2020 rajesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
